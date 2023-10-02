36c0270692bc41b7f9ef6f26f1cddfe29c9995cc
Marcel Zimmermann <mzmann252@gmail.com>
2023-10-02 12:51:20 +0200

    1 Commit

---
+++ Dart-Grundlagen/12_methoden.dart
@@ -0,0 +1,18 @@
+// Was lernen wir heute ?
+
+// Was sind Methoden und wie funktionieren Sie?
+void main() { 
+
+ print("Vor"); // Ausdruck in der Console
+ doSomething(1); // Aufruf der Methode mit einem Übergabewert
+  print("Danach");  // Ausdruck in der Console
+
+}
+
+void doSomething(int x) {  // deklaration einer Methode und dem Datentyp was wir erwarten
+
+ int y = 3; // deklaration eines Int 3
+
+ print(x+y);  // Ausdruck in der Console wo mit x und y gearbeitet wird
+
+}
