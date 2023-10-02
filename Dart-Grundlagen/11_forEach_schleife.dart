5dd6e5ec782cc1ae878cef3c138379eff6209644
Marcel Zimmermann <mzmann252@gmail.com>
2023-10-02 12:52:53 +0200

    commit2

--- Dart-Grundlagen/11_forEach_schleife.dart
+++ Dart-Grundlagen/11_forEach_schleife.dart
@@ -7,12 +7,14 @@
 // Wichtig ist zu wissen das man beim 
 // Interieren(durchlaufen des Arrays wird der aktuelle Wert zwischengespeichert 
 // und in einer Variablen gehalten)
+// gehaltene Variable kann nicht manipuliert werden 
+
 void main() { 
 
   List<int> liste = [1,2,3,4,5];
 
   liste.forEach((element) { 
-    print(element);
+    print(element); // Ausgabe in der Console das er jedes Element einmal ausdruckt
   });
 
   
